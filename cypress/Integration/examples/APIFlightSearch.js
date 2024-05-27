2c61b7d583de29916e9f52e51222d320d5d3f42f
Faraz12 <mohammad.farazkhan@gmail.com>
2024-05-27 05:35:51 +0530

    Added Assertions

--- cypress/Integration/examples/APIFlightSearch.js
+++ cypress/Integration/examples/APIFlightSearch.js
@@ -3,11 +3,16 @@ describe('Flight Search Request', function () {
   before(() => {
     cy.fixture('flightSearch').then(function (data) {
       this.data = data
-      //cy.log(JSON.stringify(this.data))
+      cy.log(JSON.stringify(this.data))
+
     })
   })
 
   it('Search Flight', function () {
+  
+    
+ 
+
   cy.request({
     method: 'POST',
     url: 'api/flights/${NumberOfSegment}',
@@ -20,17 +25,23 @@ describe('Flight Search Request', function () {
     body: this.data
 
   }).then(function (resp) {
-    cy.log(JSON.stringify(resp.body))
+    expect(resp.status).to.eq(200)
     var data = JSON.stringify(resp.body)
-    cy.log("ddfdldldld",data)
     expect(resp.body.combinabilityStrictMode).eq(true)
     expect(resp.body.segments[0].route).eq('DXB_PRG')
-    expect(resp.status).to.eq(200)
+    expect(resp.body.segments[0].origin).eq(this.data.searchCriteria[0].origin)
+    expect(resp.body.segments[0].dest).eq(this.data.searchCriteria[0].dest)
+    expect(resp.body.segments[0]).have.property('lowestAdultFarePerPax')
+    expect(resp.body.segments).to.be.an('Array')
+    expect(resp.body.lowestTotalFare).to.be.an('Array')
+    expect(resp.body.messages).to.be.an('Array')
+    expect(resp.body.validationRules).to.be.an('Object')
+    expect(resp.body.segments[0].lowestFareByCabin).to.be.an('Array')
+    expect(resp.body.segments[0].flights).to.be.an('Array')
+    expect(resp.body.segments[0].brands).to.be.an('Array')
+    expect(resp.body.segments[0].flights[0].fareTypes[0].fareInformation.adultFares[0].paxCount).eq(this.data.paxInfo.adultCount)
 
   })
-
-
-
 
 })
 
